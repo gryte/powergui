06521b23f44ba19999621f04da56d6288378d26c
Adam Linkous <alinkous@gmail.com>
2015-06-21 13:39:42 -0500

    added name for test kitchen compatibility

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license 'Apache 2.0'
 license 'All rights reserved'
 description 'Installs/Configures PowerGUI for Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name 'powergui'
 version '1.0.0'
 supports 'windows'
 depends 'windows'
