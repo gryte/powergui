24f01bb2c949f490fb3a7f5d73c1fac70c03367b
Adam Linkous <alinkous@gmail.com>
2015-07-11 20:34:41 -0500

    added windows dsl helpers to check versions; successfully converged 2012r2 and 2008r2

--- recipes/dotnet.rb
+++ recipes/dotnet.rb
@@ -1,3 +1,23 @@
-windows_feature 'NetFx3' do
-  action :install
+case node['platform']
+when 'windows'
+
+  require 'chef/win32/version'
+
+  windows_version = Chef::ReservedNames::Win32::Version.new
+
+  # Use windows cookbook to manage feature on Windows 2008r2
+  if windows_version.windows_server_2008_r2?
+    windows_feature 'NetFx3' do
+      action :install
+    end
+  end
+
+  # Use powershell resource to manage feature on Windows 2012r2
+  if windows_version.windows_server_2012_r2?
+    powershell_script 'dotNet35' do
+      code 'Install-WindowsFeature -name Net-Framework-Core'
+      guard_interpreter :powershell_script
+      not_if "(Get-WindowsFeature -Name Net-Framework-Core).Installed"
+    end
+  end
 end
