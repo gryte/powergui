9a6b0288d382179cb001ebc7718aed0945e08024
Adam Linkous <alinkous@gmail.com>
2015-07-20 23:45:12 -0500

    updated win7 image to use opentable; serverspec now just uses dism method

--- test/integration/dotnet/serverspec/dotnet_spec.rb
+++ test/integration/dotnet/serverspec/dotnet_spec.rb
@@ -1,13 +1,10 @@
 require 'serverspec'
+# require 'chef/win32/version'
 
 # Required by serverspec
 set :backend, :cmd
-set :os, :family => 'windows'
+set :os, family: 'windows'
 
 describe windows_feature('NetFx3') do
   it { should be_installed.by('dism') }
-end
-
-describe windows_feature('Net-Framework-Core') do
-  it { should be_installed.by('powershell') }
 end
